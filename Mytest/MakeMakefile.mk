60cd5c75dff6b471126573708c15f084492cd375
G4Vi <hayesgav@gmail.com>
2021-07-30 10:52:57 -0400

    simplify steps to build xs library

--- Mytest/MakeMakefile.mk
+++ Mytest/MakeMakefile.mk
@@ -1,3 +1,12 @@
+SHELL := /bin/sh
+all: Makefile
+	$(MAKE)
+
 Makefile: Makefile.PL
 	perl Makefile.PL
-    
+
+clean:
+	$(MAKE) clean || [ ! -f Makefile ]
+	rm -f Makefile
+
+.PHONY: all
--- build.pl
+++ build.pl
@@ -11,8 +11,5 @@ system('make', '-C', 'static/music_worklet_inprogress/player/') == 0 or die('fai
 # build xs library for server
 say "current binary " . $FindBin::Bin;
 my $flacdir = File::Spec->catdir($FindBin::Bin, 'Mytest');
-chdir($flacdir) or die("failed to enter flac dir");
-system('make', '-f', 'MakeMakefile.mk') == 0 or die("Failed to make MakeMakefile.mk");
-system('make') == 0 or die("Make failed");
+system('make', '-C', $flacdir, '-f', 'MakeMakefile.mk') == 0 or die("Failed to make Mytest");
 
-exit 0;
