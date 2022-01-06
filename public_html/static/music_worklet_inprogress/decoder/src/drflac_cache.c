9d1c32e8ceb64ae439e1c500be18a6ee3b80b35e
G4Vi <hayesgav@gmail.com>
2022-01-06 18:12:11 -0500

    switch to submodules inside of music_worklet_inprogress/decoder/deps. use drflac fork for now for unaligned load bug

--- public_html/static/music_worklet_inprogress/decoder/src/drflac_cache.c
+++ public_html/static/music_worklet_inprogress/decoder/src/drflac_cache.c
@@ -2,8 +2,8 @@
 #include <stdbool.h>
 #include <inttypes.h>
 
-//#define DRFLAC_IMPLEMENTATION
-//#include "dr_flac.h"
+#define DRFLAC_IMPLEMENTATION
+#include "dr_flac.h"
 
 #define MINIAUDIO_IMPLEMENTATION
 #include "miniaudio.h"
