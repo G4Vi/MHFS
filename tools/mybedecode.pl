2cde24ff1a3edfc380eb4c8ae6f4207752698cb5
G4Vi <hayesgav@gmail.com>
2022-05-09 17:11:24 -0400

    add non-recursive solution for bencode, implement more perl-like  non-resursive bdecode, add searching functions

--- tools/mybedecode.pl
+++ tools/mybedecode.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 use strict; use warnings;
 use feature 'say';
+use Data::Dumper;
 
 @ARGV >= 1 or die("no files specified");
 
@@ -25,220 +26,154 @@ sub write_file {
     close($fh);
 }
 
-use constant {
-    BDEC_DICT              => 1 << 0,
-    BDEC_LIST              => 1 << 1,
+# a node is an array with the first element being the type, followed by the value(s)
+# ('int', iv)          - integer node, MUST have one integer value, bencoded as iIVe
+# ('bstr', bytestring) - byte string node, MUST have one bytestring value, bencoded as bytestringLength:bytestring where bytestringLength is the length as ASCII numbers
+# ('l', values)        - list node, MAY have one or more values of type int, bstr, list, and dict bencoded as lVALUESe
+# ('d', kvpairs)       - dict node, special case of list, MAY one or more key and value pairs. A dict node MUST have multiple of 2 values; a bstr key with corespoding value
+# ('null', value)      - null node, MAY have one value, used internally by bdecode to avoid dealing with the base case of no parent
+# ('e')                - end node, MUST NOT have ANY values, used internally by bencode to handle writing list/dict end
 
-    BDEC_END_OK_ALWAYS     => 1 << 2,
-    BDEC_END_OK_SOMETIMES  => 1 << 3,
+sub bdecode {
+    my ($contents, $foffset) = @_;
+    my @headnode = ('null');
+    my @nodestack = (\@headnode);
+    my $startoffset = $foffset;
 
-    BDEC_GET_VAL_ALWAYS    => 1 << 4,
-    BDEC_GET_VAL_SOMETIMES => 1 << 5
-};
-
-use constant BDEC_END_OK => (BDEC_END_OK_ALWAYS | BDEC_END_OK_SOMETIMES);
-use constant BDEC_GET_VAL     => (BDEC_GET_VAL_ALWAYS | BDEC_GET_VAL_SOMETIMES);
-use constant BDEC_DICT_TOGGLE => BDEC_END_OK_SOMETIMES | BDEC_GET_VAL_SOMETIMES;
-
-use constant BDEC_DICT_GET_KEY => BDEC_DICT | BDEC_END_OK_SOMETIMES;
-use constant BDEC_DICT_GET_VAL => BDEC_DICT | BDEC_GET_VAL_SOMETIMES;
-use constant BDEC_LIST_GET_VAL => BDEC_LIST | BDEC_END_OK_ALWAYS | BDEC_GET_VAL_ALWAYS;
-
-my @statestack = (BDEC_DICT_GET_KEY);
-my $foffset = 0;
-my $headnode = ['dict', undef];
-my $curnode = $headnode;
-
-sub _node_addchild {
-    my ($node, $chld) = @_;
-    push @{$node}, $chld;
-}
-
-use Scalar::Util qw(weaken);
-sub node_addchilddict {
-    my ($node) = @_;
-    weaken($node);
-    my $dictnode = ['dict', $node];
-    _node_addchild($node, $dictnode);
-    return $dictnode;
-}
-
-sub node_addchildlist {
-    my ($node) = @_;
-    weaken($node);
-    my $listnode = ['list', $node];
-    _node_addchild($node, $listnode);
-    return $listnode;
-}
-
-sub node_addinteger {
-    my ($node, $val) = @_;
-    my $chldnode = ['int', $val];
-    _node_addchild($node, $chldnode);
-}
-
-sub node_addbstr {
-    my ($node, $val) = @_;
-    my $chldnode = ['bstr', $val];
-    _node_addchild($node, $chldnode);
-}
-
-sub node_parent {
-    my ($node) = @_;
-    return $node->[1];
-}
-
-sub indentedprint {
-    my $message = ( ' ' x (scalar(@statestack) * 4)) . $_[0];
-    say $message;
-}
-my $contents = read_file($ARGV[0]);
-
-# finally parse
-if((length($contents) == 0) || (substr($contents, $foffset++, 1) ne 'd')) {
-    say 'invalid file';
-    exit 0;
-}
-indentedprint('dict start');
-while(1) {
-    indentedprint('state ' . $statestack[-1]);
-    my $char = substr($contents, $foffset++, 1);
-    my $curstate = $statestack[-1];
-    if($curstate & BDEC_END_OK) {
-        if($char eq 'e') {
-            pop @statestack;
-            indentedprint((($curstate & BDEC_DICT) ? 'dict' : 'list') . ' end');
-            if(scalar(@statestack) == 0) {
-                last;
+    while(1) {
+        # a bstr is always valid as it can be a dict key
+        if(substr($$contents, $foffset) =~ /^(0|[1-9][0-9]*):/) {
+            my $count = $1;
+            $foffset += length($count)+1;
+            my $bstr = substr($$contents, $foffset, $count);
+            my $node = ['bstr', $bstr];
+            $foffset += $count;
+            push @{$nodestack[-1]}, $node;
+        }
+        elsif(($nodestack[-1][0] ne 'd') || ((scalar(@{$nodestack[-1]}) % 2) == 0)) {
+            my $firstchar = substr($$contents, $foffset++, 1);
+            if(($firstchar eq 'd') || ($firstchar eq 'l')) {
+                my $node = [$firstchar];
+                push @{$nodestack[-1]}, $node;
+                push @nodestack, $node;
             }
-            $curnode = node_parent($curnode);
-            next;
+            elsif(substr($$contents, $foffset-1) =~ /^i(0|\-?[1-9][0-9]*)e/) {
+                my $node = ['int', $1];
+                $foffset += length($1)+1;
+                push @{$nodestack[-1]}, $node;
+            }
+            else {
+                say "bad elm $firstchar $foffset";
+                return undef;
+            }
+        }
+        elsif((substr($$contents, $foffset, 1) eq 'e') &&
+        (scalar(@nodestack) != 1) &&
+        (($nodestack[-1][0] ne 'd') || ((scalar(@{$nodestack[-1]}) % 2) == 1)))
+        {
+            pop @nodestack;
+            $foffset++;
+        }
+        else {
+            say "bad elm $foffset";
+            return undef;
+        }
+
+        if(scalar(@nodestack) == 1) {
+            return [$headnode[1], $foffset-$startoffset];
         }
     }
-    # Change the next dictionary operation (at this level) between GET_KEY and GET_VAL (if it's a dictionary)
-    $statestack[-1] ^= BDEC_DICT_TOGGLE;
-    if($curstate & BDEC_GET_VAL) {
-        if($char eq 'd') {
-            indentedprint('dict start');
-            push @statestack, BDEC_DICT_GET_KEY;
-            $curnode = node_addchilddict($curnode);
-            next;
-        }
-        elsif($char eq 'i') {
-            my $intstr;
-            $char = substr($contents, $foffset++, 1);
-            if(($char eq '-') || ($char eq '0'))  {
-                $intstr .= $char;
-                $char = substr($contents, $foffset++, 1);
-            }
-            my $cval = ord($char);
-            if(($cval < ord('1')) || ($cval > ord('9'))) {
-                indentedprint(__LINE__ .' unexpected char '.$char);
-                last;
-            }
-            $intstr .= $char;
-            while(1) {
-                $char = substr($contents, $foffset++, 1);
-                my $cval = ord($char);
-                if(($cval < ord('0')) || ($cval > ord('9'))) {
-                    if($char ne 'e') {
-                        indentedprint(__LINE__ .'unexpected char');
-                        exit 0;
-                    }
-                    last;
-                }
-                $intstr .= $char;
-            }
-            indentedprint('int ' . $intstr);
-            node_addinteger($curnode, $intstr);
-            next;
-        }
-        elsif($char eq 'l') {
-            indentedprint('list start');
-            push @statestack, BDEC_LIST_GET_VAL;
-            $curnode = node_addchildlist($curnode);
-            next;
-        }
-    }
-
-    my $curbstr;
-    if($char eq '0') {
-        $curbstr .= $char;
-        $char = substr($contents, $foffset++, 1);
-        if($char ne ':') {
-            indentedprint(__LINE__ .'unexpected char');
-            last;
-        }
-    }
-    else {
-        while(1) {
-            my $cval = ord($char);
-            if(($cval < ord('0')) || ($cval > ord('9'))) {
-                if(($char ne ':') || (length($curbstr) == 0)) {
-                    indentedprint(__LINE__ .'unexpected char '.$char);
-                    exit 0;
-                }
-                last;
-            }
-            $curbstr .= $char;
-            $char = substr($contents, $foffset++, 1);
-        }
-    }
-    my $toprint = ('bstr '.$curbstr.' ') . (($curbstr < 100) ? substr($contents, $foffset, $curbstr) : '');
-    indentedprint($toprint);
-    node_addbstr($curnode, substr($contents, $foffset, $curbstr));
-    $foffset += $curbstr;
 }
-
-use Data::Dumper;
-
-print Dumper($headnode);
 
 sub bencode {
     my ($node) = @_;
+    my @toenc = ($node);
     my $output;
 
-    my $type = $node->[0];
-    if($type eq 'dict') {
-        $output .= 'd';
-        for(my $i = 2; $i < scalar(@{$node});) {
-            my $key = $node->[$i++];
-            $output .= bencode($key);
-            my $value = $node->[$i++];
-            $output .= bencode($value);
+    while(my $node = shift @toenc) {
+        my $type = $node->[0];
+        if(($type eq 'd') || ($type eq 'l')) {
+            $output .= $type;
+            my @nextitems = @{$node};
+            shift @nextitems;
+            push @nextitems, ['e'];
+            unshift @toenc, @nextitems;
         }
-        $output .= 'e';
-    }
-    elsif($type eq 'list') {
-        $output .= 'l';
-        for(my $i = 2; $i < scalar(@{$node}); $i++) {
-            my $value = $node->[$i];
-            $output .= bencode($value);
+        elsif($type eq 'bstr') {
+            $output .= sprintf("%u:%s", length($node->[1]), $node->[1]);
         }
-        $output .= 'e';
-    }
-    elsif($type eq 'bstr') {
-        $output .= sprintf("%u:%s", length($node->[1]), $node->[1]);
-    }
-    elsif($type eq 'int') {
-        $output .= 'i'.$node->[1].'e';
+        elsif($type eq 'int') {
+            $output .= 'i'.$node->[1].'e';
+        }
+        elsif($type eq 'e') {
+            $output .= 'e';
+        }
+        else {
+            return undef;
+        }
     }
 
     return $output;
 }
 
+sub bdictfind {
+    my ($node, $keys, $valuetype) = @_;
+    NEXTKEY: foreach my $key (@{$keys}) {
+        if($node->[0] ne 'd') {
+            say "cannot search non dictionary";
+            return undef;
+        }
+        for(my $i = 1; $i < scalar(@{$node}); $i+=2) {
+            if($node->[$i][1] eq $key) {
+                $node = $node->[$i+1];
+                last NEXTKEY;
+            }
+        }
+        say "failed to find key $key";
+        return undef;
+    }
+    if(($valuetype) && ($node->[0] ne $valuetype)) {
+        say "node has wrong type, expected $valuetype got ". $node->[0];
+        return undef;
+    }
+    return $node;
+}
+
+sub bdictgetkeys {
+    my ($node) = @_;
+    if($node->[0] ne 'd') {
+        say "cannot search non dictionary";
+        return undef;
+    }
+    my @keys;
+    for(my $i = 1; $i < scalar(@{$node}); $i+=2) {
+        push @keys, $node->[$i][1];
+    }
+    return \@keys;
+}
+
+my $contents = read_file($ARGV[0]);
+my $ret = bdecode(\$contents, 0);
+defined($ret) or die('failed to bedecode');
+say 'readlen ' . $ret->[1];
+my $headnode = $ret->[0];
+
 my $reenc = bencode($headnode);
+defined($reenc) or die('failed to bencode');
 say "reenc length " . length($reenc);
 write_file('out.torrent', $reenc);
 
-my $infohashnode;
-for(my $i = 2; $i < scalar(@{$headnode}); $i += 2) {
-    if(($headnode->[$i][1] eq 'info') && ($headnode->[$i+1][0] eq 'dict')) {
-        $infohashnode = $headnode->[$i+1];
-        say "found infohash";
-        last;
-    }
-}
-$infohashnode or die("didn't find infohahs");
+my $infohashnode = bdictfind($headnode, ['info'], 'd');
+$infohashnode or die("didn't find infohash");
 write_file('out.infohash', bencode($infohashnode));
+
+my $dkeys = bdictgetkeys($headnode);
+foreach my $key (@{$dkeys}) {
+    say "key: $key";
+}
+
+my $ikeys = bdictgetkeys($infohashnode);
+foreach my $key (@{$ikeys}) {
+    say "key: $key";
+}
 
