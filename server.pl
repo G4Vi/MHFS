6d039051546cb8a8cec2e494462d78f6d3b3dd06
G4Vi <hayesgav@gmail.com>
2022-05-13 17:19:00 -0400

    move tracker into it's own plugin, add peer timeout

--- server.pl
+++ server.pl
@@ -3869,8 +3869,216 @@ package MHFS::Plugin::Youtube {
     1;
 }
 
+package MHFS::Plugin::BitTorrent::Tracker {
+    use strict; use warnings;
+    use feature 'say';
+    use Time::HiRes qw( clock_gettime CLOCK_MONOTONIC);
+    use Data::Dumper;
+
+    sub bencode {
+        my ($node) = @_;
+        my @toenc = ($node);
+        my $output;
+
+        while(my $node = shift @toenc) {
+            my $type = $node->[0];
+            if(($type eq 'd') || ($type eq 'l')) {
+                $output .= $type;
+                my @nextitems = @{$node};
+                shift @nextitems;
+                push @nextitems, ['e'];
+                unshift @toenc, @nextitems;
+            }
+            elsif($type eq 'bstr') {
+                $output .= sprintf("%u:%s", length($node->[1]), $node->[1]);
+            }
+            elsif($type eq 'int') {
+                $output .= 'i'.$node->[1].'e';
+            }
+            elsif($type eq 'e') {
+                $output .= 'e';
+            }
+            else {
+                return undef;
+            }
+        }
+
+        return $output;
+    }
+
+    sub announce_error {
+        my ($message) = @_;
+        return ['d', ['bstr', 'failure reason'], ['bstr', $message]];
+    }
+
+    sub peertostring {
+        my ($peer) = @_;
+        my @pvals = unpack('CCCCCC', $peer);
+        return "$pvals[0].$pvals[1].$pvals[2].$pvals[3]:" . (($pvals[4] << 8) | $pvals[5]);
+    }
+
+    sub removeTorrentPeer {
+        my ($self, $infohash, $peer, $reason) = @_;
+        say __PACKAGE__.": removing torrent peer ".peertostring($peer). " - $reason";
+        delete $self->{torrents}{$infohash}{$peer};
+    }
+
+    sub announce {
+        my ($self, $request) = @_;
+
+        # hide the tracker if the required parameters aren't there
+        foreach my $key ('port', 'left', 'info_hash') {
+            if(! exists $request->{'qs'}{$key}) {
+                say __PACKAGE__.": missing $key";
+                $request->Send404;
+                return;
+            }
+        }
+
+        my $dictref;
+        while(1) {
+            my $port = $request->{'qs'}{'port'};
+            if($port ne unpack('S', pack('S', $port))) {
+                $dictref = announce_error("bad port");
+                last;
+            }
+            my $left = $request->{'qs'}{'left'};
+            if($left ne unpack('Q', pack('Q', $left))) {
+                $dictref = announce_error("bad left");
+                last;
+            }
+            if(exists $request->{'qs'}{'compact'} && ($request->{'qs'}{'compact'} eq '0')) {
+                $dictref = announce_error("Only compact responses supported!");
+                last;
+            }
+
+            my $rih = $request->{'qs'}{'info_hash'};
+            if(!exists $self->{torrents}{$rih}) {
+                $dictref = announce_error("The torrent does not exist!");
+                last;
+            }
+
+            my $ip = $request->{'ip'};
+            my $ipport = pack('Nn', $ip, $port);
+            say __PACKAGE__.": announce from ".peertostring($ipport);
+
+
+            my $event = $request->{'qs'}{'event'};
+            if( (! exists $self->{torrents}{$rih}{$ipport}) &&
+            ((! defined $event) || ($event ne 'started'))) {
+                $dictref = announce_error("first announce must include started event");
+                last;
+            }
+
+            if($left == 0) {
+                $self->{torrents}{$rih}{$ipport}{'completed'} = 1;
+            }
+
+            $self->{torrents}{$rih}{$ipport}{'last_announce'} = clock_gettime(CLOCK_MONOTONIC);
+
+            if(defined $event) {
+                say __PACKAGE__.": announce event $event";
+                if($event eq 'started') {
+                    $self->{torrents}{$rih}{$ipport} = {'exists' => 1};
+                }
+                elsif($event eq 'stopped') {
+                    $self->removeTorrentPeer($rih, $ipport, " received stopped message");
+                }
+                elsif($event eq 'completed') {
+                    #$self->{torrents}{$rih}{$ipport}{'completed'} = 1;
+                }
+            }
+
+            my $numwant = $request->{'qs'}{'numwant'};
+            if((! defined $numwant) || ($numwant ne unpack('C', pack('C', $numwant))) || ($numwant > 55)) {
+                $numwant = 50;
+            }
+
+            my @dict = ('d');
+            push @dict, ['bstr', 'interval'], ['int', $self->{'announce_interval'}];
+            my $complete = 0;
+            my $incomplete = 0;
+            my $pstr = '';
+            my $i = 0;
+            foreach my $peer (keys %{$self->{torrents}{$rih}}) {
+                if($self->{torrents}{$rih}{$peer}{'completed'}) {
+                    $complete++;
+                }
+                else {
+                    $incomplete++;
+                }
+                if($i++ < $numwant) {
+                    if($peer ne $ipport) {
+                        my @values = unpack('CCCCCC', $peer);
+                        my $netmap = $request->{'client'}{'server'}{'settings'}{'NETMAP'};
+                        my $pubip = $request->{'client'}{'server'}{'settings'}{'PUBLICIP'};
+                        if($netmap && (($values[0] == $netmap->[1]) && (unpack('C', $ipport) != $netmap->[1])) && $pubip) {
+                            say "HACK converting local peer to public ip";
+                            $peer = pack('Nn', MHFS::Util::ParseIPv4($pubip), (($values[4] << 8) | $values[5]));
+                        }
+                        say __PACKAGE__.": sending peer ".peertostring($peer);
+                        $pstr .= $peer;
+                    }
+                }
+            }
+            #push @dict, ['bstr', 'complete'], ['int', $complete];
+            #push @dict, ['bstr', 'incomplete'], ['int', $incomplete];
+            push @dict, ['bstr', 'peers'], ['bstr', $pstr];
+
+            $dictref = \@dict;
+            last;
+        }
+
+        # bencode and send
+        my $bdata = bencode($dictref);
+        if($bdata) {
+            $request->SendBytes('text/plain', $bdata);
+        }
+        else {
+            say "Critical: Failed to bencode!";
+            $request->Send404;
+        }
+    }
+
+    sub new {
+        my ($class, $settings) = @_;
+        my $ai = ($settings->{'BitTorrent::Tracker'} && $settings->{'BitTorrent::Tracker'}{'announce_interval'}) ? $settings->{'BitTorrent::Tracker'}{'announce_interval'} : undef;
+        $ai //= 1800;
+        my $self =  {'settings' => $settings, 'torrents' => \%{$settings->{'TORRENTS'}}, 'announce_interval' => $ai};
+        bless $self, $class;
+        say __PACKAGE__.": announce interval: ".$self->{'announce_interval'};
+
+        $self->{'routes'} = [
+        ['/torrent/tracker', sub {
+            my ($request) = @_;
+            $self->announce($request);
+        }],
+        ];
+
+        $self->{'timers'} = [
+            # once an hour evict peers that left the swarm ungracefully
+            [0, 3600, sub {
+                my ($timer, $current_time, $evp) = @_;
+                say __PACKAGE__.": evict peers timer";
+                foreach my $infohash (keys %{$self->{'torrents'}}) {
+                    foreach my $peer (keys %{$self->{'torrents'}{$infohash}}) {
+                        if(($current_time - $peer->{'last_announce'}) > ($self->{'announce_interval'}+60)) {
+                            $self->removeTorrentPeer($infohash, $peer, " timeout");
+                        }
+                    }
+                }
+                return 1;
+            }],
+        ];
+
+        return $self;
+    }
+
+    1;
+}
+
 package App::MHFS; #Media Http File Server
-use version; our $VERSION = version->declare("v0.1.0");
+use version; our $VERSION = version->declare("v0.2.0");
 unless (caller) {
 use strict; use warnings;
 no warnings "portable";
@@ -3930,8 +4138,6 @@ if(scalar(@ARGV) >= 1 ) {
 # load settings
 my $SETTINGS = MHFS::Settings::load(abs_path(__FILE__));
 
-my %TORRENTS = %{$SETTINGS->{'TORRENTS'}};
-
 my %RESOURCES; # Caching of resources
 
 # make the temp dirs
@@ -3941,7 +4147,7 @@ make_path($SETTINGS->{'SECRET_TMPDIR'}, {chmod => 0600});
 # load plugins
 my @plugins;
 {
-    my @plugintotry = ('MHFS::Plugin::Youtube');
+    my @plugintotry = ('MHFS::Plugin::Youtube', 'MHFS::Plugin::BitTorrent::Tracker');
     push (@plugintotry, 'MHFS::Plugin::MusicLibrary') if($SETTINGS->{'MusicLibrary'});
     foreach my $plugin (@plugintotry) {
         next if(defined $SETTINGS->{$plugin}{'enabled'} && (!$SETTINGS->{$plugin}{'enabled'}));
@@ -4031,9 +4237,6 @@ my @routes = (
             }
             $request->Send404;
         }
-    ],
-    [
-        '/torrent/tracker', \&tracker
     ],
     [
         '/torrent', \&torrent
@@ -7164,156 +7367,6 @@ sub torrent {
         });
     }
     else {
-        $request->Send404;
-    }
-}
-
-sub bencode {
-    my ($node) = @_;
-    my @toenc = ($node);
-    my $output;
-
-    while(my $node = shift @toenc) {
-        my $type = $node->[0];
-        if(($type eq 'd') || ($type eq 'l')) {
-            $output .= $type;
-            my @nextitems = @{$node};
-            shift @nextitems;
-            push @nextitems, ['e'];
-            unshift @toenc, @nextitems;
-        }
-        elsif($type eq 'bstr') {
-            $output .= sprintf("%u:%s", length($node->[1]), $node->[1]);
-        }
-        elsif($type eq 'int') {
-            $output .= 'i'.$node->[1].'e';
-        }
-        elsif($type eq 'e') {
-            $output .= 'e';
-        }
-        else {
-            return undef;
-        }
-    }
-
-    return $output;
-}
-
-sub tracker_error {
-    my ($message) = @_;
-    return ['d', ['bstr', 'failure reason'], ['bstr', $message]];
-}
-
-sub tracker {
-    my ($request) = @_;
-
-    # hide the tracker if the required parameters aren't there
-    foreach my $key ('port', 'left', 'info_hash') {
-        if(! exists $request->{'qs'}{$key}) {
-            $request->Send404;
-            return;
-        }
-    }
-
-    my $dictref;
-    while(1) {
-        my $port = $request->{'qs'}{'port'};
-        if($port ne unpack('S', pack('S', $port))) {
-            $dictref = tracker_error("bad port");
-            last;
-        }
-        my $left = $request->{'qs'}{'left'};
-        if($left ne unpack('Q', pack('Q', $left))) {
-            $dictref = tracker_error("bad left");
-            last;
-        }
-        if(exists $request->{'qs'}{'compact'} && ($request->{'qs'}{'compact'} eq '0')) {
-            $dictref = tracker_error("Only compact responses supported!");
-            last;
-        }
-
-        my $rih = $request->{'qs'}{'info_hash'};
-        if(!exists $TORRENTS{$rih}) {
-            $dictref = tracker_error("The torrent does not exist!");
-            last;
-        }
-
-        my $ip = $request->{'ip'};
-        my $ipport = pack('Nn', $ip, $port);
-        my @pvals = unpack('CCCCCC', $ipport);
-        say "announce ipport: $pvals[0].$pvals[1].$pvals[2].$pvals[3] at port " . (($pvals[4] << 8) | $pvals[5]);
-
-        my $event = $request->{'qs'}{'event'};
-        if( (! exists $TORRENTS{$rih}{$ipport}) &&
-        ((! defined $event) || ($event ne 'started'))) {
-            $dictref = tracker_error("first announce must include started event");
-            last;
-        }
-
-        if($left == 0) {
-            $TORRENTS{$rih}{$ipport}{'completed'} = 1;
-        }
-
-        if(defined $event) {
-            if($event eq 'started') {
-                $TORRENTS{$rih}{$ipport} = {'exists' => 1};
-            }
-            elsif($event eq 'stopped') {
-                delete $TORRENTS{$rih}{$ipport};
-            }
-            elsif($event eq 'completed') {
-                #$TORRENTS{$rih}{$ipport}{'completed'} = 1;
-            }
-        }
-
-        my $numwant = $request->{'qs'}{'numwant'};
-        if((! defined $numwant) || ($numwant ne unpack('C', pack('C', $numwant))) || ($numwant > 55)) {
-            $numwant = 50;
-        }
-
-        my @dict = ('d');
-        push @dict, ['bstr', 'interval'], ['int', 120];
-        my $complete = 0;
-        my $incomplete = 0;
-        my $pstr = '';
-        my $i = 0;
-        foreach my $peer (keys %{$TORRENTS{$rih}}) {
-            if($TORRENTS{$rih}{$peer}{'completed'}) {
-                $complete++;
-            }
-            else {
-                $incomplete++;
-            }
-            if($i++ < $numwant) {
-                if($peer ne $ipport) {
-                    my @values = unpack('CCCCCC', $peer);
-                    my $netmap = $request->{'client'}{'server'}{'settings'}{'NETMAP'};
-                    my $pubip = $request->{'client'}{'server'}{'settings'}{'PUBLICIP'};
-                    if($netmap && (($values[0] == $netmap->[1]) && (unpack('C', $ipport) != $netmap->[1])) && $pubip) {
-                        say "HACK converting local peer to public ip";
-                        $peer = pack('Nn', MHFS::Util::ParseIPv4($pubip), (($values[4] << 8) | $values[5]));
-                        @values = unpack('CCCCCC', $peer);
-                    }
-                    say "sending peer $values[0].$values[1].$values[2].$values[3] at port " . (($values[4] << 8) | $values[5]);
-                    $pstr .= $peer;
-                }
-            }
-        }
-        #push @dict, ['bstr', 'complete'], ['int', $complete];
-        #push @dict, ['bstr', 'incomplete'], ['int', $incomplete];
-        push @dict, ['bstr', 'peers'], ['bstr', $pstr];
-
-        $dictref = \@dict;
-        last;
-    }
-
-    # bencode and send
-    my $bdata = bencode($dictref);
-    if($bdata) {
-        $request->SendBytes('text/plain', $bdata);
-    }
-    else {
-        say "Critical: Failed to bencode!";
         $request->Send404;
     }
 }
