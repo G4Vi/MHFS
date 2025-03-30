ed2f2204c086ffe1619cd35cade750c93657de75
G4Vi <hayesgav@gmail.com>
2025-03-30 18:11:25 -0400

    fix circular dependencies with MHFS::Kodi::* and MHFS::Plugin::Kodi

--- App-MHFS/lib/MHFS/Kodi/MovieEdition.pm
+++ App-MHFS/lib/MHFS/Kodi/MovieEdition.pm
@@ -1,10 +1,25 @@
 package MHFS::Kodi::MovieEdition v0.7.0;
 use 5.014;
 use strict; use warnings;
+use File::Basename qw(basename);
+use MHFS::Kodi::Util qw(html_list_item);
+use MHFS::Kodi::MoviePart;
+use MHFS::Util qw(str_to_base64url);
+
+sub Format {
+    my ($sourcename, $editionname, $ediition) = @_;
+    my @sortedkeys = sort {basename($a) cmp basename($b)} keys %$ediition;
+    my @parts = map {
+        MHFS::Kodi::MoviePart::Format($editionname, $_, $ediition->{$_})
+    } @sortedkeys;
+    my $editionid = "$sourcename/".str_to_base64url($editionname);
+    my %edition = ( id => $editionid, name => basename($editionname), parts => \@parts);
+    \%edition
+}
 
 sub TO_JSON {
     my ($self) = @_;
-    {edition => MHFS::Plugin::Kodi::_format_movie_edition($self->{source}, $self->{editionname}, $self->{edition})}
+    {edition => Format($self->{source}, $self->{editionname}, $self->{edition})}
 }
 
 sub TO_HTML {
@@ -12,7 +27,7 @@ sub TO_HTML {
     my $parts = $self->TO_JSON()->{edition}{parts};
     my $buf = '<style>ul{list-style: none;} li{margin: 10px 0;}</style><ul>';
     foreach my $part (@$parts) {
-        $buf .= MHFS::Plugin::Kodi::_html_list_item($part->{id}, 1, $part->{name});
+        $buf .= html_list_item($part->{id}, 1, $part->{name});
     }
     $buf .= '</ul>';
     $buf
